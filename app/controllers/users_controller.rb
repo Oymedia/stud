c6b35d07d8c3105e58a9ab9fcf059a6c281de03b
oymedia <oymediasolutions@gmail.com>
2022-02-24 13:13:02 +0530

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,37 @@
 class UsersController < ApplicationController
 
     def new
-    @user = User.new
+        @user = User.new
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+        
+      end
+
+    def create
+            @user = User.new(user_params)
+
+        if @user.save
+            redirect_to @user, notice: 'Welcome to e-kali blog, you have successfully signuped'
+        else
+            render :new, status: :unprocessable_entity      
+        end
     end
 
-
-def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to @user, notice: 'Welcome to e-kali blog, you have successfully signuped'
-    else
-      render :new, status: :unprocessable_entity      
-    end
-  end
+    def update
+        @user = User.find(params[:id])
+       
+        if @user.update(user_params)
+           redirect_to @user, notice: 'Your account information was updated successfully'
+          else
+            render :edit, status: :unprocessable_entity
+        end
+      end
 
 private
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 
 end
